5ddc911de4336853b5be94d467538dd991d308a1
Noah Strekow <163929425+ParadoxZ-FGC@users.noreply.github.com>
2025-03-24 19:06:28 -0400

    Create organize.py

---
+++ organize.py
@@ -0,0 +1,59 @@
+# Author : Noah Strekow
+# Description : Sorts the files in target directory 
+
+import os
+
+
+# Store file locations
+script_dir = os.getcwd()
+target = "C:/Users/NOST/Downloads/"
+target_dir = os.fsencode(target)
+
+# Folders & Dictionary of file extensions
+folders = ["C:/Users/NOST/Documents/", "C:/Users/NOST/Pictures/", "C:/Users/NOST/Videos/", "C:/Users/NOST/Music/"]
+file_exts = {
+    # Documents
+    'pdf': folders[0],
+    'doc': folders[0],
+    'docx': folders[0],
+    'txt': folders[0],
+    'log': folders[0],
+    # Data
+    'csv': folders[0],
+    'xlsx': folders[0],
+    # Archives
+    'zip': folders[0],
+    'rar': folders[0],
+    '7z': folders[0],
+    # Images
+    'png': folders[1],
+    'jpg': folders[1],
+    'jpeg': folders[1],
+    'gif': folders[1],
+    'svg': folders[1],
+    'webp': folders[1],
+    # Videos
+    'mp4': folders[2],
+    'flv': folders[2],
+    'avi': folders[2],
+    'wmv': folders[2],
+    'webm': folders[2],
+    # Audio
+    'mp3': folders[3],
+    'wav': folders[3],
+    'ogg': folders[3],
+    # Leave others in Downloads
+}
+
+
+# Check file type for every file in target directory, move file to proper subfolder
+if __name__ == '__main__':
+    for file in os.listdir(target_dir):
+        filename = os.fsdecode(file)
+        filename_split = filename.split('.')
+        if len(filename_split) == 1:
+            continue    # If no extension, skip
+        ext = filename_split[len(filename_split)-1]   # Grab extension
+        dest = file_exts.get(ext, "C:/Users/NOST/Downloads/")   # Find where file should go according to extension, defualt = Misc
+        print(f'{target_dir}, {filename}, {ext}, {dest}')
+        os.rename(f"{target}{filename}", f"{dest}{filename}")    # Move file 
